cc85cc0ca4c07a7f34eccca8b791ad6928d55378
James Sobeck <sobeckjames@gmail.com>
2022-04-13 22:52:13 -0500

    added css for background

---
+++ frontend/src/layout/Home.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import Container from 'react-bootstrap/Container';
+
+export default function Home(){
+    return (
+        <div >
+           
+                test
+            
+        </div>
+    )
+}
